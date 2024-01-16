164349dc3bd2d57a6cb111903cf795fa9c8cfda9
asad200-OK <asadullakhan0098@gmail.com>
2024-01-16 20:41:07 +0530

    Add getRandomRGB function
    this function returns a random rgb value as a string

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ const gridContainer = document.querySelector('.grid-container');
 
 const resetBtn = document.querySelector('button');
 
+function getRandomRGB() {
+    const rgbValues = [];
+    const randomRGBValue = [];
+    for(let i = 1; i < 256; i++) {
+        rgbValues.push(i);
+    }
+    for(let i = 0; i < 3; i++) {
+        let randomNumber = Math.floor(Math.random() * rgbValues.length);
+        randomRGBValue.push(rgbValues[randomNumber]);
+    }
+    return `rgb(${randomRGBValue.join(',')})`;
+}
