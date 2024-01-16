5faf903555fec7e9dccccec4956dfa121e42d13b
asad200-OK <asadullakhan0098@gmail.com>
2024-01-16 20:31:39 +0530

    Select 4 elements & store them in variables
    selected elements: input, p, .grid-container and button

---
+++ script.js
@@ -0,0 +1,8 @@
+const inputEl = document.querySelector('input');
+
+const para = document.querySelector('p');
+
+const gridContainer = document.querySelector('.grid-container');
+
+const resetBtn = document.querySelector('button');
+
